99120282866ef33e5f7fb28af4e6282c08033d25
dnikitha123 <136118487+dnikitha123@users.noreply.github.com>
2023-07-04 22:39:20 +0530

    hello

---
+++ src/components/Hero.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import me from "../assets/images/me.jpg";
+
+
+const Hero = () => {
+  const social_media = [
+    "logo-instagram",
+    "logo-facebook",
+    "logo-linkedin",
+    "logo-twitter",
+  ];
+  return (
+    <section
+      id="home"
+      className="min-h-screen flex py-10 md:flex-row flex-col items-center"
+    >
+      <div className="flex-1 flex items-center justify-center">
+        <img src={me} alt="" className=" w-72 h-96  rounded object-cover" />
+      </div>
+      <div className="flex-1">
+        <div className="md:text-left text-center">
+          <h1 className="md:text-5xl text-2xl md:leading-normal leading-10 text-white font-bold">
+            <span className="text-cyan-600 md:text-6xl text-5xl">
+              Hello!
+              <br />
+            </span>
+            My Name is <span> Akhila</span>
+          </h1>
+          <h4 className="md:text-2xl text-lg md:leading-normal leading-5 mt-4 font-bold text-gray-600">
+            Web3 Frontend Developer
+          </h4>
+          <button className="btn-primary mt-8">Contact Me</button>
+          <div className="mt-8 text-3xl flex items-center md:justify-start justify-center gap-5">
+            {social_media?.map((icon) => (
+              <div
+                key={icon}
+                className="text-gray-600 hover:text-white cursor-pointer "
+              >
+                <ion-icon name={icon}></ion-icon>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
